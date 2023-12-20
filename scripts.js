87d1e3dce5e667e7983645745c88dcadd6788914
Seth Feaser <Sfeaser@cs.washington.edu>
2023-12-20 15:29:35 -0800

    updating dropdown list function

--- scripts.js
+++ scripts.js
@@ -3,7 +3,7 @@ function playAudio() {
   var jingle = document.getElementById("jingle");
   var voice = document.getElementById("voice");
 
-  var selectedVoice = document.getElementById("voice").value;
+  var selectedVoice = document.getElementById("voice-select").value;
   voice.src = "sound-effects/voices/" + selectedVoice;
 
   jingle.play();
