d9c678b7ba689d9b5c25e2d126e292d612085961
Seth Feaser <Sfeaser@cs.washington.edu>
2023-12-20 15:21:32 -0800

    replacing voices

--- scripts.js
+++ scripts.js
@@ -2,6 +2,10 @@
 function playAudio() {
   var jingle = document.getElementById("jingle");
   var voice = document.getElementById("voice");
+
+  var selectedVoice = document.getElementById("voice").value;
+  voice.src = "sound-effects/voices/" + selectedVoice;
+
   jingle.play();
   voice.play();
 }
