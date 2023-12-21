fb7da988adc8c0e6dc8132a983d4f575a18ce945
Seth Feaser <Sfeaser@cs.washington.edu>
2023-12-21 13:40:12 -0800

    fully functional version commit

--- scripts.js
+++ scripts.js
@@ -1,13 +1,34 @@
+timer = null
+
+function getSelectedVoice() {
+  return "sound-effects/voices/" + document.getElementById("voice-select").value;
+}
+
+function resetAudio() {
+  if (timer) clearTimeout(timer);
+  document.getElementById("jingle").load();
+  document.getElementById("voice").load();
+}
+
+function playVoice() {
+  document.getElementById("voice").src = getSelectedVoice();
+  document.getElementById("voice").play();
+}
+
+function playJingle() {
+  document.getElementById("jingle").play();
+}
 
 function playAudio() {
-  var jingleSound = document.getElementById("jingle");
-  var voiceSound = document.getElementById("voice");
-
-  var selectedVoice = document.getElementById("voice-select").value;
-  voiceSound.src = "sound-effects/voices/" + selectedVoice;
-
-  jingleSound.play();
-  voiceSound.play();
+  resetAudio();
+  playJingle();
+  timer = setTimeout(() => {
+    playVoice();
+    clearTimeout(timer);
+  }, 1000);
 }
+
+document.getElementById('main-btn').addEventListener('click', playAudio);
+document.getElementById('voice-select').addEventListener('change', resetAudio);
 
 
